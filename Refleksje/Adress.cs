670c087521cd2154300c71bbd70f0ad8db1b9322
Fi Szer <filipsz60@wp.pl>
2023-05-16 13:53:21 +0200

    Dodaj pliki projektów.

---
+++ Refleksje/Adress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refleksje
+{
+
+        public class Address
+        {
+            public string City { get; set; }
+            public string Street { get; set; }
+        [DisplayProperty("Postal Code")]
+            public string PostalCode { get; set; }
+        }
+    
+}
---
+++ Refleksje/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+
+namespace Refleksje
+{
+    class Program
+    {
+        static void Display(object obj)
+        {
+            Type objType = obj.GetType();
+            var properties = objType.GetProperties();
+
+            foreach (var property in properties)
+            {
+                var propValue = property.GetValue(obj);
+                var propType = propValue.GetType();
+                if (propType.IsPrimitive || propType == typeof(string))
+                {
+                    var displayPropertyAttribute = property.GetCustomAttribute<DisplayPropertyAttribute>();
+                    if (displayPropertyAttribute != null)
+                        Console.WriteLine($"property {displayPropertyAttribute.DisplayName}: {propValue}");
+                    else
+                        Console.WriteLine($"property {property.Name}: {propValue}");
+
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Address address = new Address()
+            {
+                City = "Krakow",
+                PostalCode = "31-556",
+                Street = "Grodzka 5"
+            };
+
+            Person person = new Person()
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Address = address
+            };
+            Display(person);
+            Display(person.Address);
+
+            Console.WriteLine("\r\nInsert person property to change:");
+            var propertyToUpdate = Console.ReadLine();
+            Console.WriteLine("\r\nInsert value to change:");
+            var valueToUpdate = Console.ReadLine();
+
+
+            setValue(person, propertyToUpdate, valueToUpdate);
+
+            Display(person);
+            Display(person.Address);
+        }
+        public static void setValue<T>(T obj, string prop, string value)
+        {
+            Type objType = obj.GetType();
+            var properties = objType.GetProperty(prop);
+
+            if (properties != null)
+            {
+                properties.SetValue(obj, value);
+            }
+        }
+    }
+}
